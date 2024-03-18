5233f2b2ceb3346c9d061e30195992e6815198c4
Yeison Villegas <yvillegas@plm.com.co>
2024-03-18 16:13:02 -0500

    feature(app): Agregada aplicacion post al administrador

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('createdAt', 'updatedAt') # Para mostrar estos elementos pero sin ser editables
+    list_display = ('title', 'createdAt', 'updatedAt') # Para ver que columnas puedo ver
+    date_hierarchy = 'createdAt'
+
+admin.site.register(Post, PostAdmin) # Esto se usa para extender la configuracion del administrador y registrar el modulo post en el administrador
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    createdAt = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    updatedAt = models.DateTimeField(auto_now=True, verbose_name="Modificado")
+
+    def __str__(self) -> str: # Esto es para que los print que se ven en el administrador sobre la clase post siempre se vean como el titulo
+        return self.title
+    
+    class Meta:
+        verbose_name = "entrada"
+        verbose_name_plural = "entradas"
